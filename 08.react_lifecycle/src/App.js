45292c337c6253cc160c4302bb74186a59b9dbfa
신현진 <blackduvet52@gmail.com>
2023-09-04 17:51:08 +0900

    react_life

--- 08.react_lifecycle/src/App.js
+++ 08.react_lifecycle/src/App.js
@@ -1,7 +1,13 @@
 import './App.css'
-
+import RCComponent from './components/RCComponent'
+import RFComponent from './components/RFComponent'
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <RCComponent></RCComponent>
+      <RFComponent></RFComponent>
+    </div>
+  )
 }
 
 export default App
