c808511eb637a4ae5c6015b322e9a7908d1e3b06
신현진 <blackduvet52@gmail.com>
2023-09-06 17:16:27 +0900

    Update ProductDetail2.jsx

--- 09.react_router/src/components/ProductDetail2.jsx
+++ 09.react_router/src/components/ProductDetail2.jsx
@@ -3,6 +3,8 @@ import { useSearchParams } from "react-router-dom";
 
 const ProductDetail2 = () => {
   const [query, setQuery] = useSearchParams();
+
+  // cate에 연결
   console.log("Product페이지로부터 전달받은 값", query.get("cate"));
   return <div>ProductDetail2</div>;
 };
